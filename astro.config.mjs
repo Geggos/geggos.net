bc51a10e6c932ad7307dc1cd3e1240da8897925f
alex buh <alex@buh.moe>
2026-03-13 11:57:41 +0100

    initial geggos layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://geggos.net",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
